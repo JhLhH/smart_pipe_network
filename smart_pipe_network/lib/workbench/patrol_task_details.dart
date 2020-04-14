6bbf7f43d36da4678c0ab63154be9d993578fe7a
李俊恒 <ljh@weiyian.com>
2020-04-14 16:23:03 +0800

    完成巡查任务未完成模块的详情页面

--- smart_pipe_network/lib/workbench/patrol_task_details.dart
+++ smart_pipe_network/lib/workbench/patrol_task_details.dart
@@ -11,7 +11,15 @@ class PatrolTaskDetailsPage extends StatefulWidget {
 }
 
 class _PatrolTaskDetailsPageState extends State<PatrolTaskDetailsPage> {
-
+    final List<String> items = [
+      '任务名称：2020.02.11巢湖路至护航路（含护航路）巡查',
+      '周期：1周/次',
+      '起始时间：2019-05-11 至 2019-12-30',
+      '安排时间：2019-05-10 09:30',
+      '派发人：李书长',
+      '任务状态：进行中',
+      '备注：无'
+    ];
    bool _isTapStart = false;
    bool _isTapEnd = false;
   @override
@@ -32,8 +40,26 @@ class _PatrolTaskDetailsPageState extends State<PatrolTaskDetailsPage> {
   _getListItemBuilder(int index){
     if (index == 0){
       return _getTopWidget();
+    }else if(index == 1){
+      return _getBodyWidget();
     }
-    return Center(child: Text('暂无数据'),);
+    return
+      GestureDetector(
+        onTap: (){
+          // 病害上报
+          print('病害上报点击');
+        },
+      child: Container(
+        margin: EdgeInsets.only(left: 20,right: 20,top: 40),
+        height: 49,
+        alignment: Alignment.center,
+        child: Text('病害上报',style: TextStyle(color: Colors.white,fontSize: 18, fontWeight: FontWeight.w500),),
+        decoration: BoxDecoration(
+          color: Colors.orangeAccent,
+          borderRadius: BorderRadius.circular(24.5),
+        ),
+      ),
+    );
   }
 
   /// 获取顶部两个按钮组件
@@ -76,6 +102,68 @@ class _PatrolTaskDetailsPageState extends State<PatrolTaskDetailsPage> {
               border: Border.all(color: Colors.white,width: 1),
               borderRadius: BorderRadius.circular(50)
           ),
+        ),
+      ),
+    );
+  }
+
+
+  /// body内容
+  _getBodyWidget(){
+    return  Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.white, width: 1),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          margin: EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: _getBodyDetails(),
+          ),
+        )
+      ],
+    );
+  }
+
+  _getBodyDetails(){
+    List<Widget> widgetItems = [];
+    items.forEach((text) {
+      widgetItems.add(_getBodyText(text));
+    });
+    widgetItems.insert(0, _getBodyText('任务编号：RW20191102001',color: Colors.grey));
+    widgetItems.add(
+      GestureDetector(
+        onTap: (){
+          print('巡查路径点击');
+        },
+        child: Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.fromLTRB(10, 5, 30, 5),
+          child: Text(
+            '巡查路径',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+                color: Colors.blue
+            ),
+          ),
+        ),
+      )
+    );
+    return widgetItems;
+  }
+
+  _getBodyText(String text,{Color color = Colors.black}){
+   return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.fromLTRB(10, 5, 30, 5),
+      child: Text(
+        text,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          color: color
         ),
       ),
     );
